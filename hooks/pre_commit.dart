9247814b5952afbdd7ea673a19e2c8efefd1c2e8
Morgan <mrgnhnt96@gmail.com>
2024-12-05 01:30:44 -0600

    feat: improve hooks and add tests

--- hooks/pre_commit.dart
+++ hooks/pre_commit.dart
@@ -6,14 +6,22 @@ Hook main() {
       ReRegisterHooks(),
       ShellScript(
         pathPatterns: [Glob('**.dart')],
+        excludePatterns: [Glob('**.g.dart')],
         commands: (files) => [
           'dart analyze --fatal-infos ${files.join(' ')}',
         ],
       ),
       ShellScript(
-        pathPatterns: [Glob('**.dart')],
+        pathPatterns: [Glob('**/models/**.dart')],
+        excludePatterns: [Glob('**.g.dart')],
         commands: (files) => [
           'sip run build_runner build',
+        ],
+      ),
+      ShellScript(
+        pathPatterns: [Glob('**.dart')],
+        commands: (files) => [
+          'sip test --concurrent --bail',
         ],
       ),
     ],
