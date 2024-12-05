0d69881c8abda86c7360de781f7d683780f33de1
Morgan <mrgnhnt96@gmail.com>
2024-12-05 01:31:53 -0600

    feat: exclude hooks dir

--- hooks/pre_commit.dart
+++ hooks/pre_commit.dart
@@ -6,20 +6,27 @@ Hook main() {
       ReRegisterHooks(),
       ShellScript(
         pathPatterns: [Glob('**.dart')],
-        excludePatterns: [Glob('**.g.dart')],
+        excludePatterns: [
+          Glob('**.g.dart'),
+          Glob('hooks/**'),
+        ],
         commands: (files) => [
           'dart analyze --fatal-infos ${files.join(' ')}',
         ],
       ),
       ShellScript(
         pathPatterns: [Glob('**/models/**.dart')],
-        excludePatterns: [Glob('**.g.dart')],
+        excludePatterns: [
+          Glob('**.g.dart'),
+          Glob('hooks/**'),
+        ],
         commands: (files) => [
           'sip run build_runner build',
         ],
       ),
       ShellScript(
         pathPatterns: [Glob('**.dart')],
+        excludePatterns: [Glob('hooks/**')],
         commands: (files) => [
           'sip test --concurrent --bail',
         ],
