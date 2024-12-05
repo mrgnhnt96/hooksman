3a744815d3501fcfffe819a7935885e0d64ab8cc
Morgan <mrgnhnt96@gmail.com>
2024-12-05 01:25:01 -0600

    feat: extract run logic to hook executor class

--- lib/entrypoint/execute_hook.dart
+++ lib/entrypoint/execute_hook.dart
@@ -2,18 +2,25 @@ import 'dart:async';
 import 'dart:io';
 
 import 'package:file/local.dart';
-import 'package:git_hooks/entrypoint/hook_execution/label_maker.dart';
-import 'package:git_hooks/entrypoint/hook_execution/pending_tasks.dart';
+import 'package:git_hooks/entrypoint/hook_execution/hook_executor.dart';
 import 'package:git_hooks/models/hook.dart';
 import 'package:git_hooks/models/resolver.dart';
 import 'package:git_hooks/services/git_service.dart';
-import 'package:git_hooks/utils/multi_line_progress.dart';
 import 'package:mason_logger/mason_logger.dart';
 
 Future<void> executeHook(String name, Hook hook) async {
   const fs = LocalFileSystem();
 
-  final logger = Logger()..level = Level.verbose;
+  final level = switch (Platform.environment['LOG_LEVEL']) {
+    'loud' || 'all' || 'verbose' => Level.verbose,
+    'info' => Level.info,
+    'warning' => Level.warning,
+    'error' => Level.error,
+    'critical' => Level.critical,
+    _ => Level.info,
+  };
+
+  final logger = Logger()..level = level;
   final gitService = GitService(
     logger: logger,
     fs: fs,
@@ -25,84 +32,19 @@ Future<void> executeHook(String name, Hook hook) async {
   );
 
   try {
-    logger.info('starting...');
-    exitCode = await run(
+    final executor = HookExecutor(
       hook,
+      stdout: stdout,
       hookName: name,
       logger: logger,
       gitService: gitService,
       resolver: resolver,
     );
+
+    exitCode = await executor.run();
   } catch (e) {
     exitCode = 1;
   }
 
   exit(exitCode);
-}
-
-Future<int> run(
-  Hook hook, {
-  required String hookName,
-  required Logger logger,
-  required GitService gitService,
-  required Resolver resolver,
-}) async {
-  logger.info('Running $hookName hook');
-
-  final allFiles = await gitService.getChangedFiles(hook.diff);
-
-  if (allFiles == null) {
-    logger.err('Could not get changed files');
-    return 1;
-  }
-
-  if (allFiles.isEmpty) {
-    logger.info('No files to process');
-    return 0;
-  }
-
-  final resolvedHook = resolver.resolve(allFiles);
-
-  final pendingTasks = PendingTasks(
-    resolvedHook,
-    logger: logger,
-  );
-
-  if (pendingTasks.tasks.every((e) => e.files.isEmpty)) {
-    logger.info('No matching files');
-    return 0;
-  }
-
-  final labelMaker = LabelMaker(
-    stdout: stdout,
-    tasks: pendingTasks.tasks,
-    nameOfHook: hookName,
-  );
-
-  final progress = MultiLineProgress(createLabel: labelMaker.create)..start();
-
-  pendingTasks.start();
-
-  await pendingTasks.wait();
-
-  if (pendingTasks.wasKilled) {
-    progress
-      ..dispose()
-      ..print();
-    return 1;
-  }
-
-  await progress.closeNextFrame();
-
-  logger
-    ..flush()
-    ..write('\n');
-
-  for (final task in pendingTasks.tasks) {
-    if (task.code case final int code when code != 0) {
-      return code;
-    }
-  }
-
-  return 0;
 }
---
+++ lib/entrypoint/hook_execution/hook_executor.dart
@@ -0,0 +1,88 @@
+import 'dart:io' as io;
+
+import 'package:git_hooks/entrypoint/hook_execution/label_maker.dart';
+import 'package:git_hooks/entrypoint/hook_execution/pending_tasks.dart';
+import 'package:git_hooks/models/hook.dart';
+import 'package:git_hooks/models/resolver.dart';
+import 'package:git_hooks/services/git_service.dart';
+import 'package:git_hooks/utils/multi_line_progress.dart';
+import 'package:mason_logger/mason_logger.dart';
+
+class HookExecutor {
+  const HookExecutor(
+    this.hook, {
+    required this.stdout,
+    required this.hookName,
+    required this.logger,
+    required this.gitService,
+    required this.resolver,
+  });
+
+  final Hook hook;
+  final io.Stdout stdout;
+  final String hookName;
+  final Logger logger;
+  final GitService gitService;
+  final Resolver resolver;
+
+  Future<int> run() async {
+    logger.info('Running $hookName hook');
+
+    final allFiles = await gitService.getChangedFiles(hook.diff);
+
+    if (allFiles == null) {
+      logger.err('Could not get changed files');
+      return 1;
+    }
+
+    if (allFiles.isEmpty) {
+      logger.info('No files to process');
+      return 0;
+    }
+
+    final resolvedHook = resolver.resolve(allFiles);
+
+    final pendingTasks = PendingTasks(
+      resolvedHook,
+      logger: logger,
+    );
+
+    if (pendingTasks.tasks.every((e) => e.files.isEmpty)) {
+      logger.info('No matching files');
+      return 0;
+    }
+
+    final labelMaker = LabelMaker(
+      stdout: stdout,
+      tasks: pendingTasks.tasks,
+      nameOfHook: hookName,
+    );
+
+    final progress = MultiLineProgress(createLabel: labelMaker.create)..start();
+
+    pendingTasks.start();
+
+    await pendingTasks.wait();
+
+    if (pendingTasks.wasKilled) {
+      progress
+        ..dispose()
+        ..print();
+      return 1;
+    }
+
+    await progress.closeNextFrame();
+
+    logger
+      ..flush()
+      ..write('\n');
+
+    for (final task in pendingTasks.tasks) {
+      if (task.code case final int code when code != 0) {
+        return code;
+      }
+    }
+
+    return 0;
+  }
+}
