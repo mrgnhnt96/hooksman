98a939b3a568622102a5ab742991198e92a4e5f5
Morgan <mrgnhnt96@gmail.com>
2024-12-07 19:00:21 -0700

    update

--- hooks/pre_commit.dart
+++ hooks/pre_commit.dart
@@ -3,34 +3,64 @@ import 'package:hooksman/hooksman.dart';
 Hook main() {
   return Hook(
     tasks: [
-      ReRegisterHooks(),
-      ShellTask(
-        name: 'Lint & Format',
-        include: [Glob('**.dart')],
-        exclude: [
-          Glob('**.g.dart'),
-        ],
-        commands: (files) => [
-          'dart analyze --fatal-infos ${files.join(' ')}',
-          'dart format ${files.join(' ')}',
-        ],
-      ),
-      ShellTask(
+      // ReRegisterHooks(),
+      // ShellTask(
+      //   name: 'Lint & Format',
+      //   include: [Glob('**.dart')],
+      //   exclude: [
+      //     Glob('**.g.dart'),
+      //   ],
+      //   commands: (files) => [
+      //     'dart analyze --fatal-infos ${files.join(' ')}',
+      //     'dart format ${files.join(' ')}',
+      //   ],
+      // ),
+      // ShellTask(
+      //   name: 'Build Runner',
+      //   include: [Glob('lib/models/**.dart')],
+      //   exclude: [Glob('**.g.dart')],
+      //   commands: (files) => [
+      //     'sip run build_runner build',
+      //   ],
+      // ),
+      ParallelTasks(
         name: 'Build Runner',
-        include: [Glob('lib/models/**.dart')],
-        exclude: [Glob('**.g.dart')],
-        commands: (files) => [
-          'sip run build_runner build',
-        ],
-      ),
-      ShellTask(
-        name: 'Tests',
         include: [Glob('**.dart')],
-        exclude: [Glob('hooks/**')],
-        commands: (files) => [
-          'sip test --concurrent --bail',
+        tasks: [
+          ShellTask(
+            include: [AllFiles()],
+            commands: (files) => [
+              'exit 1',
+            ],
+          ),
+          ShellTask(
+            include: [AllFiles()],
+            commands: (files) => [
+              'sleep 1',
+            ],
+          ),
+          ShellTask(
+            include: [AllFiles()],
+            commands: (files) => [
+              'sleep 1',
+            ],
+          ),
+          ShellTask(
+            include: [AllFiles()],
+            commands: (files) => [
+              'sleep 1',
+            ],
+          ),
         ],
       ),
+      // ShellTask(
+      //   name: 'Tests',
+      //   include: [Glob('**.dart')],
+      //   exclude: [Glob('hooks/**')],
+      //   commands: (files) => [
+      //     'sip test --concurrent --bail',
+      //   ],
+      // ),
     ],
   );
 }
