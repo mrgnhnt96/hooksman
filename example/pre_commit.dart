007a5afd515e78dc79023037f650b60919ae46a7
Morgan <mrgnhnt96@gmail.com>
2024-12-09 08:22:44 -0700

    feat: add example

---
+++ example/pre_commit.dart
@@ -0,0 +1,36 @@
+import 'package:hooksman/hooksman.dart';
+
+Hook main() {
+  return Hook(
+    tasks: [
+      ReRegisterHooks(),
+      ShellTask(
+        name: 'Lint & Format',
+        include: [Glob('**.dart')],
+        exclude: [
+          Glob('**.g.dart'),
+        ],
+        commands: (files) => [
+          'dart analyze --fatal-infos ${files.join(' ')}',
+          'dart format ${files.join(' ')}',
+        ],
+      ),
+      ShellTask(
+        name: 'Build Runner',
+        include: [Glob('lib/models/**.dart')],
+        exclude: [Glob('**.g.dart')],
+        commands: (files) => [
+          'sip run build_runner build',
+        ],
+      ),
+      ShellTask(
+        name: 'Tests',
+        include: [Glob('**.dart')],
+        exclude: [Glob('hooks/**')],
+        commands: (files) => [
+          'sip test --concurrent --bail',
+        ],
+      ),
+    ],
+  );
+}
